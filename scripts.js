b9ce3b945dcc2200ee8b53c24166dbe345f99b22
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-02-14 16:09:15 -0300

    correção de tratamento do erro caso o nome já exist

--- scripts.js
+++ scripts.js
@@ -1,22 +1,47 @@
 const userName = {
-    name: 'galinho xico liro'
+    name: ''
 }
 
-const participants = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', userName);
-const messages = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+function yourName() {
+    userName.name = '';
+    userName.name = prompt("Qual seu nome?");
 
-messages.then(searchMessages);
-setInterval(updateStatus, 5000);
+    if (userName.name === '') {
+        yourName();
+    }
+    const userParticipant = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', userName);
+    
+    userParticipant.then(allowedName);
+    userParticipant.catch(nameAlreadyExists)
+
+}
+
+
+function allowedName(userParticipant) {
+    console.log(userParticipant.status)
+    console.log(userParticipant);
+    setInterval(updateStatus, 5000);
+    
+    const messages = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    messages.then(searchMessages);
+}
+
+function nameAlreadyExists(erro) {
+    console.log(erro.response.status)
+    if (erro.response.status === 400) {
+        alert("nome já existe!!");
+        yourName();
+    }
+}
 
 function updateStatus() {
-    axios.post('https://mock-api.driven.com.br/api/v4/uol/status',userName);
-    // userActive.then(ola);
+    let teste = axios.post('https://mock-api.driven.com.br/api/v4/uol/status',userName);
+    teste.then(teste2);
 
-    // function ola(userActive) {
-    //     console.log(userActive);
-    // }
+    function teste2(teste) {
+        console.log(teste.status);
+    }
 }
-
 
 function openCloseModal() {
     let selected = document.querySelector('section.modal');
@@ -28,6 +53,8 @@ function openCloseModal() {
         selected.classList.remove('active');
     }
 }
+
+
 
 function searchMessages(messages) {
     let main = document.querySelector('main');
@@ -44,7 +71,6 @@ function searchMessages(messages) {
             </div>
         `
     }
-    console.log(messages)
 }
 
 function sendMessage(button) {
@@ -60,4 +86,22 @@ function sendMessage(button) {
         axios.post('https://mock-api.driven.com.br/api/v4/uol/messages', message);
     }
     document.querySelector('.send-message-container input').value = "";
+}
+
+function showParticipants(participants) {
+    let contacts = document.querySelector('.container-contacts');
+    let participant = participants.data
+    console.log(participant)
+
+    for (let i = 0; i < participant.length; i++) {
+        contacts.innerHTML = contacts.innerHTML + `
+        <div class="contact active" onclick="selectContact()">
+            <div>
+                <ion-icon name="person-circle"></ion-icon>
+                <p>${participant[i].name}</p>
+            </div>
+            <ion-icon name="checkmark-sharp"></ion-icon>
+        </div>
+        `
+    }
 }
